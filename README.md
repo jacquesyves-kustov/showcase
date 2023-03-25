2233bd24316e0dd975831fede6799df5e914bacd
sergei <sergekust@gmail.com>
2023-03-25 21:37:04 +0500

    Add docker and API entrypoint

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # showcase
 Here's my asynchronous-python-backend-showcase-project :)
+
+Used techs:
+- FastAPI
--- src/__main__.py
+++ src/__main__.py
@@ -1,2 +1,18 @@
+import argparse
+import asyncio
+
+from api.app import run_server
+
+
+async def startup(mode: str):
+    if mode == 'backend':
+        await run_server()
+
+
 if __name__ == '__main__':
-    print('Hello world!')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode', help='Run a special container.', choices=['backend'])
+    args = parser.parse_args()
+
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(startup(args.mode))
