f6cb5bd75b9a6bfef5a486832e62462e24feaa93
sergei <sergekust@gmail.com>
2023-03-26 16:35:45 +0500

    Introduce alembic

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Here's my asynchronous-python-backend-showcase-project :)
 
 Used techs:
 - FastAPI
+- SQLAlchemy 2.0
+- alembic
+- PostgreSQL 
---
+++ migrations/versions/2344e387ed48_first_migration.py
@@ -0,0 +1,20 @@
+"""first_migration
+
+Revision ID: 2344e387ed48
+Revises: 
+Create Date: 2023-03-26 16:23:10.831318
+
+"""
+# revision identifiers, used by Alembic.
+revision = '2344e387ed48'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
